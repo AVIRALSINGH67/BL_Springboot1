6b83feb1ee6012d0e6ef300cf42e3eba5219d39c
AVIRALSINGH67 <aviral.raghuvansh1411@gmail.com>
2025-03-01 23:13:53 +0530

    UC2 Updated

--- src/main/java/com/example/SpringConcepts/HelloController.java
+++ src/main/java/com/example/SpringConcepts/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.SpringConcepts;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,15 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+}
+
+
+@RestController
+@RequestMapping("/hello")
+class mark {
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
